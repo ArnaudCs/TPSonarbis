d39c140cefca5efeeea3206de058a42454bf9357
Adam S <adam.said@etu.umontpellier.fr>
2021-12-11 12:46:31 +0100

    fix code smells

--- src/main/java/pizzas/BasePizzas.java
+++ src/main/java/pizzas/BasePizzas.java
@@ -31,33 +31,24 @@ public class BasePizzas {
 	}
 	
 	private void initMenu() {
-		Ingredient i1=new Ingredient("sauce tomate", true);
-		Ingredient i2=new Ingredient("creme fraiche", true);
-		Ingredient i3=new Ingredient("jambon blanc", false);
-		Ingredient i4=new Ingredient("aubergine", true);
-		Ingredient i5=new Ingredient("mozzarelle", true);
-		Ingredient i6=new Ingredient("pancetta", false);
-		Ingredient i7=new Ingredient("champignons", true);
-		Ingredient i8=new Ingredient("courgette", true);
-		Ingredient i9=new Ingredient("taleggio", true);
-		Ingredient i10=new Ingredient("chorizo", false);
+		this.initIngredients();
 		Pizza p1=new Pizza("berk", 10);
-		p1.ajoutIngredient(i8);
-		p1.ajoutIngredient(i10);
+		p1.ajoutIngredient(ingredientsDisponibles.get(7));
+		p1.ajoutIngredient(ingredientsDisponibles.get(9));
 		Pizza p2=new Pizza("vege", 11);
-		p2.ajoutIngredient(i1);
-		p2.ajoutIngredient(i4);
-		p2.ajoutIngredient(i5);
-		p2.ajoutIngredient(i7);
-		p2.ajoutIngredient(i8);
+		p2.ajoutIngredient(ingredientsDisponibles.get(0));
+		p2.ajoutIngredient(ingredientsDisponibles.get(3));
+		p2.ajoutIngredient(ingredientsDisponibles.get(4));
+		p2.ajoutIngredient(ingredientsDisponibles.get(6));
+		p2.ajoutIngredient(ingredientsDisponibles.get(7));
 		Pizza p3=new Pizza("ouch", 15);
-		p3.ajoutIngredient(i2);
-		p3.ajoutIngredient(i3);
-		p3.ajoutIngredient(i4);
-		p3.ajoutIngredient(i5);
-		p3.ajoutIngredient(i6);
-		p3.ajoutIngredient(i9);
-		p3.ajoutIngredient(i10);
+		p3.ajoutIngredient(ingredientsDisponibles.get(1));
+		p3.ajoutIngredient(ingredientsDisponibles.get(2));
+		p3.ajoutIngredient(ingredientsDisponibles.get(3));
+		p3.ajoutIngredient(ingredientsDisponibles.get(4));
+		p3.ajoutIngredient(ingredientsDisponibles.get(5));
+		p3.ajoutIngredient(ingredientsDisponibles.get(8));
+		p3.ajoutIngredient(ingredientsDisponibles.get(9));
 		menu.put(p1.getNom(), p1);
 		menu.put(p2.getNom(), p2);
 		menu.put(p3.getNom(), p3);
